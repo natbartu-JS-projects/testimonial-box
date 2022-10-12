c89ce42ef47a17006d703671b2df1576648a5542
natacha <nat_bartu@hotmail.com>
2022-10-12 12:24:21 +1000

    add functionality with JavaScript function and setInterval between change data

--- src/script.js
+++ src/script.js
@@ -50,3 +50,22 @@ const testimonials = [
     text: "This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.",
   },
 ];
+
+let idx = 1;
+
+function updateTestimonial() {
+  const { name, position, photo, text } = testimonials[idx];
+
+  testimonial.innerHTML = text;
+  userImage.src = photo;
+  username.innerHTML = name;
+  role.innerHTML = position;
+
+  idx++;
+
+  if (idx > testimonial.length - 1) {
+    idx = 0;
+  }
+}
+
+setInterval(updateTestimonial, 10000);
